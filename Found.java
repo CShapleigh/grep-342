8c27946ac296752651ae9c3aece4bc77b381dc03
Piper Chester <piperchester@gmail.com>
2015-10-28 13:40:25 -0400

    Flesh out Found

--- Found.java
+++ Found.java
@@ -1,14 +1,28 @@
 /**
  * Created by piper on 10/28/15.
  */
-
 import java.util.ArrayList;
 
 public class Found {
-    private String file_name;
-    private ArrayList<String> matching_lines_in_file;
+    private String fileName;
+    private ArrayList<String> matchingLinesInFile;
 
-    public Found() {
+    public Found(String fileName) {
         // TODO: implement
+        this.fileName = fileName;
+        this.matchingLinesInFile = new ArrayList<>();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public ArrayList<String> getMatchingLinesInFile() {
+        return matchingLinesInFile;
+    }
+
+
+    public void addLine(String line) {
+        matchingLinesInFile.add(line);
     }
 }

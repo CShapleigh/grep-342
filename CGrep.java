b2c33aeac30c9cdb5b7f1ba0fdd4964aad403663
Charles Shapleigh <CShapleigh@gmail.com>
2015-10-30 12:58:00 -0400

    Whitespace

--- CGrep.java
+++ CGrep.java
@@ -5,13 +5,13 @@ public class CGrep {
   public static void main(String [] args) {
 	  final String pattern = args[0];
 	  ArrayList<String> files = new ArrayList<String>();
-	  for(int i = 1; i < args.length; i++){
+	  for (int i = 1; i < args.length; i++) {
 		  files.add(args[i]);
 	  }
 	  final ExecutorService pool = Executors.newFixedThreadPool(3);
 	  final ExecutorCompletionService<Found> ecs = new ExecutorCompletionService<>(pool);
 
-	  for(final String file : files){
+	  for (final String file : files) {
 		  ecs.submit(new Callable<Found>() {
 			  @Override
 			  public Found call() throws Exception {
@@ -21,15 +21,15 @@ public class CGrep {
 			  }
 		  });
 	  }
-    
+
     pool.shutdown();
 
-	  for(int i = 0; i < files.size(); i++){
+	  for(int i = 0; i < files.size(); i++) {
 		  try {
 			final Future<Found> future = ecs.take();
 			final Found found = future.get();
 			System.out.println(found.getFileName());
-			for(String line : found.getMatchingLinesInFile()){
+			for (String line : found.getMatchingLinesInFile()) {
 				System.out.println(line);
 			}
 		} catch (InterruptedException | ExecutionException e) {
